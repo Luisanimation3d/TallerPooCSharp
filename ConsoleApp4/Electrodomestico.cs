6aa3700dcd8a55f9f0adc1e86c9281b17da36bea
Luis Angel Correa Ramírez <38234337+Luisanimation3d@users.noreply.github.com>
2023-07-31 11:31:53 -0500

    Add files via upload

---
+++ ConsoleApp4/Electrodomestico.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Electrodomestico
+    {
+        public double _precioBase = 100;
+        public enum _colorValidate
+        {
+            blanco, 
+            negro, 
+            rojo, 
+            azul,
+            gris
+        };
+        public enum _consumoEnergeticoValidate
+        {
+            A,
+            B,
+            C,
+            D,
+            E,
+            F
+        }
+        public double _peso = 5;
+        public string _color = "blanco";
+        public char _consumoEnergetico = 'F';
+
+        public Electrodomestico()
+        {
+        }
+
+        public Electrodomestico(double precioBase, double peso)
+        {
+            _precioBase = precioBase;
+            _peso = peso;
+        }
+
+        public Electrodomestico(double precioBase, double peso, string color, char consumoEnergetico) : this(precioBase, peso)
+        {
+            comprobarConsumoEnergetico(Char.ToUpper(consumoEnergetico));
+            comprobarColor(color.ToLower());
+        }
+
+        private void comprobarConsumoEnergetico(char letra)
+        {
+            if (Enum.IsDefined(typeof(_consumoEnergeticoValidate), letra))
+            {
+                _consumoEnergetico = letra; 
+            }
+            else
+            {
+                _consumoEnergetico = 'F';
+            }
+        }
+
+        private void comprobarColor(String color)
+        {
+            if(Enum.IsDefined(typeof(_colorValidate), _color))
+            {
+                _color = color;
+            }
+            else
+            {
+                _color = "blanco";
+            }
+        }
+
+        public void precioFinal()
+        {
+            switch (_consumoEnergetico)
+            {
+                case 'A':
+                    {
+                        _precioBase += 100;
+                        break;
+                    }
+                case 'B':
+                    {
+                        _precioBase += 80;
+                        break;
+                    }
+                case 'C':
+                    {
+                        _precioBase += 60;
+                        break;
+                    }
+                case 'D':
+                    {
+                        _precioBase += 50;
+                        break;
+                    }
+                case 'E':
+                    {
+                        _precioBase += 30;
+                        break;
+                    }
+                case 'F':
+                    {
+                        _precioBase += 10;
+                        break;
+                    }
+            }
+            if(_peso > 0 && _peso <= 19)
+            {
+                _precioBase += 10;
+            }else if(_peso >= 20 && _peso <= 49)
+            {
+                _precioBase += 50;
+            }else if(_peso >= 50 && _peso <= 79)
+            {
+                _precioBase += 80;
+            }else if(_peso >= 80)
+            {
+                _precioBase += 100;
+            }
+        }
+    }
+}
---
+++ ConsoleApp4/Lavadora.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Lavadora: Electrodomestico
+    {
+        public double _carga = 5;
+
+        public Lavadora() { }
+        public Lavadora(double precio, double peso) : base(precio, peso){ }
+        public Lavadora(double carga, double precioBase, double peso, string color, char consumoEnergetico)
+            :base(precioBase, peso, color, consumoEnergetico)
+        {
+            _carga = carga;
+        }
+
+        public void precioFinal()
+        {
+            this.precioFinal();
+            if(_carga > 30)
+            {
+                this._precioBase += 50;
+            }
+        }
+    }
+}
